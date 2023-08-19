84725faa7fd13fdd5b91adbbbca42217e2c4daf6
Edgar Pastrana <134170491+Pastrick12@users.noreply.github.com>
2023-08-19 13:46:12 -0700

    Add files via upload

---
+++ programacion/Form_Ilusiones2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgramacionOrientadaObjetos
+{
+    public partial class Form_Ilusiones2 : Form
+    {
+        Pen[] colores = { new Pen(Color.Red), new Pen(Color.Yellow), new Pen(Color.Green), new Pen(Color.Blue), new Pen(Color.Fuchsia), new Pen(Color.Aqua), new Pen(Color.Sienna), new Pen(Color.Brown) };
+        public Form_Ilusiones2()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            int w = pictureBox1.Width;
+            int h = pictureBox1.Height;
+
+            for (int i = 0; i < w/2; i += 10)
+            {
+                e.Graphics.DrawLine(colores[0], 0, i, i, h/2);
+                e.Graphics.DrawLine(colores[0], i, 0, w/2, i);
+
+                e.Graphics.DrawLine(colores[0], w/2 + i, 0, w/2, h/2 - i);
+                e.Graphics.DrawLine(colores[0], w, i, w - i, h/2);
+
+                e.Graphics.DrawLine(colores[0], 0, h/2 + i, w/2 - i, h/2);
+                e.Graphics.DrawLine(colores[0], i, h, w/2, h - i);
+
+                e.Graphics.DrawLine(colores[0], w/2, h/2 + i, w/2 + i , h);
+                e.Graphics.DrawLine(colores[0], w, h/2 + i, w/2 + i, h/2);
+            }
+        }
+    }
+}
